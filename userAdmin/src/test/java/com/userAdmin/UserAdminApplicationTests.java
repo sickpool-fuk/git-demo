021212fe2f5eb0694687448747a38eee82a0e823
sickpool <zs117127@qq.com>
2025-05-07 22:49:57 +0800

    kuangjia

---
+++ userAdmin/src/test/java/com/userAdmin/UserAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
